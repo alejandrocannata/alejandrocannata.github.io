010a259266c017211ef42f23ef50cca04047574a
alejandrocannata <126637749+alejandrocannata@users.noreply.github.com>
2023-03-03 18:41:14 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,27 @@ const menu = document.querySelector('nav ul.menu');
 menuToggle.addEventListener('click', function() {
 	menu.classList.toggle('active');
 });
+
+/*
+implementación de un boton para volver al inicio
+*/
+
+
+
+window.onscroll = function() {
+	scrollFunction();
+  };
+  
+  function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	  document.getElementById("volver-arriba").style.display = "block";
+	} else {
+	  document.getElementById("volver-arriba").style.display = "none";
+	}
+  }
+  
+  document.getElementById("volver-arriba").onclick = function() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+  };
+  
